52e7c15555368ea96e6aaba123e1ca963c3af7e8
anzani-hun <jerikostreet@gmail.com>
2024-02-15 18:06:34 +0100

    #001

--- KonferenciaProjekt/Eloadas.cs
+++ KonferenciaProjekt/Eloadas.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
 
 namespace KonferenciaProjekt
 {
@@ -12,5 +14,27 @@ namespace KonferenciaProjekt
         int sorDb;
         int helyDb;
         int[,] ertekeles;
+        //itt constructor generálása//
+
+
+
+        //az adattagokhoz készítsen get property-ket //
+        public string Cim { get => cim; set => cim = value; }
+        public int SorDb { get => sorDb; set => sorDb = value; }
+        public int HelyDb { get => helyDb; set => helyDb = value; }
+        public int[,] Ertekeles { get => ertekeles; set => ertekeles = value; }
+
+
+
+        //paraméteres konstruktor létrejött, amely a paraméterek értékével inicializálja az adattagokat  //
+        public Eloadas(string cim, int sorDb, int helyDb, int[,] ertekeles)
+        {
+            this.cim = cim;
+            this.sorDb = sorDb;
+            this.helyDb = helyDb;
+            this.ertekeles = ertekeles;
+        }
+
+
     }
 }
---
+++ KonferenciaProjekt/Konferencia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonferenciaProjekt
+{
+    internal class Konferencia
+    {
+
+        // az előadások adattag egy "ELOADAS" típusú objektumokat tartalmazó lista, ami lehetővé teszi a konferencia összes előadás kezelését
+        List<Eloadas> eloadasok;
+        public List<Eloadas> Eloadasok { get => eloadasok; }
+
+
+        public Konferencia()
+        {
+            eloadasok = new List<Eloadas>();
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    
+}
